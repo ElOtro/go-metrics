b1d2341ca4fccc28d89b2483e7654b3b3c6cbf11
Dmitry Petrunichkin <dvpetrunichkin@activebc.ru>
2022-07-10 15:20:43 +0300

    Add metrics

---
+++ cmd/agent/sender.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Run senging metrics for each type (gauge, counter)
+func (app *application) postMetricsHandler() {
+	cfg := app.config
+	client := app.client
+
+	var interval = time.Duration(app.config.reportInterval) * time.Second
+	for {
+		<-time.After(interval)
+
+		// sending gauge metrics
+		for k, v := range app.stats.Gauges {
+			url := fmt.Sprintf("http://%s:%d/%s/%s/%s/%.2f", cfg.collectorSrv.address, cfg.collectorSrv.port, "update", "gauge", k, v)
+
+			_, err := client.Post(url, "text/plain", nil)
+			if err != nil {
+				fmt.Println("Error")
+			}
+		}
+
+		// sending counter metrics
+		for k, v := range app.stats.Counters {
+			url := fmt.Sprintf("http://%s:%d/%s/%s/%s/%d", cfg.collectorSrv.address, cfg.collectorSrv.port, "update", "counter", k, v)
+
+			_, err := client.Post(url, "text/plain", nil)
+			if err != nil {
+				fmt.Println("Error")
+			}
+		}
+
+	}
+
+}
