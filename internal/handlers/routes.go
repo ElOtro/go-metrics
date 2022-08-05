04a618034cfc447be1bc9c29b5f43e0a3e746af9
ElOtro <dmitryvp.spb@gmail.com>
2022-08-05 21:03:36 +0300

    added compress middleware

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -28,6 +28,7 @@ func (h *Handlers) Routes() *chi.Mux {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(middleware.Compress(5))
 	// r.Use(app.getQueryParams)
 
 	// RESTy routes for "articles" resource
