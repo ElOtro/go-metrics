846ccff89bfda56369e2431ade42e5c21280985c
ElOtro <dmitryvp.spb@gmail.com>
2022-08-15 15:44:31 +0300

    try to fix

--- internal/config/serverConfig.go
+++ internal/config/serverConfig.go
@@ -43,7 +43,7 @@ func NewServerConfig() (*ServerEnvConfig, error) {
 	// Read the value of the port and env command-line flags into the config struct.
 	flag.Var(addr, "a", "Metrics server address host:port")
 	flag.DurationVar(&cfg.storeInterval, "i", time.Duration(300*time.Second), "pollInterval duration in seconds")
-	flag.StringVar(&cfg.storeFile, "f", "/tmp/devops-metrics-db.json", "json filename to store metrics")
+	flag.StringVar(&cfg.storeFile, "f", "", "json filename to store metrics")
 	flag.BoolVar(&cfg.restore, "r", true, "restore from json file")
 	// Key for sha256
 	// test key is "2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b"
